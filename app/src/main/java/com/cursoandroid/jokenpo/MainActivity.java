eeabcf4c37ba86a6bc1c37af86954bb32c4b9635
ClaytonEduard <clayton_eduard@hotmail.com>
2023-02-14 14:13:19 -0300

    Criando iterações visuais

--- app/src/main/java/com/cursoandroid/jokenpo/MainActivity.java
+++ app/src/main/java/com/cursoandroid/jokenpo/MainActivity.java
@@ -3,6 +3,10 @@ package com.cursoandroid.jokenpo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void selecionadoPedra(View view) {
+        this.opcaoSelecionada("pedra");
+    }
+
+    public void selecionadoPapel(View view) {
+        this.opcaoSelecionada("papel");
+    }
+
+    public void selecionadoTesoura(View view) {
+        this.opcaoSelecionada("tesoura");
+    }
+
+    public void opcaoSelecionada(String opcaoSelecionada) {
+
+        ImageView imageResultado = findViewById(R.id.imageView);
+
+        int numero = new Random().nextInt(3);//0 1 2 gera um numero entre 0 e 2
+        String[] opcoes = {"pedra", "papel", "tesoura"};
+        String opcaoApp = opcoes[numero]; // grava na variavel a palavra correspondente a posssição no array
+
+        // modo de mostra a imagem escolhida pelo App
+        switch (opcaoApp) {
+            case "pedra":
+                imageResultado.setImageResource(R.drawable.pedra);
+                break;
+            case "papel":
+                imageResultado.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                imageResultado.setImageResource(R.drawable.tesoura);
+                break;
+        }
+
+
+    }
+
+
 }
